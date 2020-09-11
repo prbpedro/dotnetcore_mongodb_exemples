7f9371109c9a89d9dae1148f43aa7cb1d69a225c
0820543660 <prbpedro@gmail.com>
2020-09-11 16:18:19 -0300

    MongoDB.Exemples

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB.Exemples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("aula_puc");
+
+            db.DropCollection("albuns");
+            db.CreateCollection("albuns");
+
+            var collection = db.GetCollection<BsonDocument>("albuns");
+
+
+            collection.Find<BsonDocument>(new BsonDocument()).ToList().ForEach(x => Console.WriteLine(x.ToString()));
+        }
+    }
+}
